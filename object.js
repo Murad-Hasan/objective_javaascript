c0f993c9f94807f45a2b7d5575c479e1bf1350c7
Murad-Hasan <muradhasan.7845@gmail.com>
2021-12-05 21:12:38 +0600

    Object

---
+++ object.js
@@ -0,0 +1,13 @@
+//new profile object
+let profile = {
+    name: "MURAD HASAN",
+    age: 20,
+}
+// console.log(profile.name);
+
+const person = new Object();
+person.name = "Murad";
+person.age = 20;
+person.color = "blue";
+
+console.log(person)
